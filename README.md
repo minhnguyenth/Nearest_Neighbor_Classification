1a3397b1ff61f83769cd94505083c3f37b0c9dac
minhnguyenxd <nguyenanhminhxd@gmail.com>
2018-02-24 11:43:25 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Face recognition using nearest neighbor method
---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+/* 
+ * File:   main.cpp
+ * Author: mnguyen
+ *
+ * Created on February 23, 2018, 6:58 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
